305ca68c2bfefef9cf8314d2fa72136a022c88d2
ayush kaurav <akaurav21@gmail.com>
2023-02-12 18:23:43 +0530

    I removed the logic

--- prime.js
+++ prime.js
@@ -7,4 +7,4 @@ if(num%i==0){
 factor++
 }
 }
-(factor==0)?console.log(" it is a prime number"):console.log(" it is not a prime number");
+(factor==0)?console.log(" it is a prime number"):console.log(" it is not  prime number");
